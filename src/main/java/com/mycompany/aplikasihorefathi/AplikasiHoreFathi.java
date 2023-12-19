9aae0b30591091034316617d2c132e85ce2522f9
fathifathan <fathifathan30@gmail.com>
2023-12-19 13:56:53 +0700

    commit 1

---
+++ src/main/java/com/mycompany/aplikasihorefathi/AplikasiHoreFathi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasihorefathi;
+
+/**
+ *
+ * @author HP
+ */
+public class AplikasiHoreFathi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        login l = new login();
+        l.setLocationRelativeTo(null);
+        l.setVisible(true);
+    }
+}
